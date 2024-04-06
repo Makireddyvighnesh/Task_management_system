320bbb1920cdc22919ca21d595348bba36401d2b
=maki7389 <vighneshmakireddy@gmail.com>
2024-04-06 12:12:27 +0530

    added environment variables

--- server/controllers/notificationController.js
+++ server/controllers/notificationController.js
@@ -1,10 +1,10 @@
 import nodemailer from 'nodemailer';
 import { google } from 'googleapis'; // Import the google object from googleapis
 
-const CLIENT_ID = CLIENT_ID;
-const CLIENT_SECRET = CLIENT_SECRET;
-const REDIRECT_URI = REDIRECT_URI;
-const REFRESH_TOKEN = REFRESH_TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 
